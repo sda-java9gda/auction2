514e7895bd8a6efd6768f129071118a0299fa208
Damian Hinc <hinc.damian@wp.pl>
2018-04-21 12:58:07 +0200

    AuctionUserView

--- src/main/java/view/AuctionView.java
+++ src/main/java/view/AuctionView.java
@@ -1,4 +1,25 @@
 package view;
 
+import models.Auction;
+import models.User;
+
+import java.util.List;
+
 public class AuctionView {
+    public static String printAuctions(List<Auction> auctions){
+
+        StringBuilder sb = new StringBuilder();
+        for (Auction auction : auctions) {
+            StringBuilder append = sb.append(auction.getName()).append("\n");
+        }
+        return sb.toString();
+    }
+    public static String printNotFound(String name) {
+        return "Brak aukcji " + name;
+    }
+
+    public static String printAddSuccess(String name) { return "Dodano aukcje " + name; }
+
+    public static String printSaledSuccess(String name) { return "Aukcja zakończona " + name; }
 }
+
--- src/main/java/view/UserView.java
+++ src/main/java/view/UserView.java
@@ -1,5 +1,34 @@
 package view;
 
+import models.User;
+
+import java.util.List;
+
 public class UserView {
+    public static String printUsers(List<User> users) {
+
+        StringBuilder sb = new StringBuilder();
+        for (User user : users) {
+            StringBuilder append = sb.append(user.getLogin()).append("\n");
+        }
+        return sb.toString();
+
+    }
+
+    public static String printLoginSuccess(String login) {
+        return "Dzien dobry " + login;
+    }
+
+    public static String printNotFound(String login) {
+        return "Brak uzytkownika " + login;
+    }
+
+    public static String printAddSuccess(String login) { return "Dodano uzytkownika " + login; }
+
+    public static String printDuplicateFound(String login) { return "Uzytkownik " + login + " juz istnieje."; }
+
+    public static String printRemovedSuccess(String login) { return "Usunieto uzytkownika " + login; }
+
+
 
 }
