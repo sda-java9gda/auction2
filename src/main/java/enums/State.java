bcd0c379072a72b3890f472c51edf55c50334cb6
Adrian.Pomykala <pomykalaadrian@gmail.com>
2018-04-21 10:50:55 +0200

    "Szkielet"

---
+++ src/main/java/enums/State.java
@@ -0,0 +1,17 @@
+package enums;
+
+public enum State {
+    INIT,
+
+    LOGGING,
+    CREATING,
+
+    LOGGED,
+
+    AUCTION_CREATE,
+    AUCTION_LIST,
+    AUCTION_OFFER,
+
+    SAVE_DATA,
+    EXIT,
+}
